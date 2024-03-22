e6bbd49bc0fbb2059f20e7b88d065bf512880650
Linda Forlizzi <la.for.lizzi@gmail.com>
2024-03-22 16:44:49 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+import dayjs from 'dayjs'
+
+// define target date variable for 10/27/
+const targetDate = dayjs('Saturday October 26, 2024 23:59:59')
+console.log(targetDate.format('dddd MMMM DD, YYYY HH:mm:ss'))
+
+// define current date variable
+
+
+
+// calculate remaining days
+  // const timeDifference = targetDate.getTime() - currentDate.getTime()
+  // console.log(timeDifference)
+
+// calculate the time difference in milliseconds
+
+
+// convert the time difference to days
+
+
+// update countdown container
