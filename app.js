71896a241557373dadf66c326159c618a6824d3e
Linda Forlizzi <la.for.lizzi@gmail.com>
2024-04-01 15:58:39 -0400

    mock for hour and minute accuracy

--- app.js
+++ app.js
@@ -3,16 +3,34 @@ document.addEventListener('DOMContentLoaded', function(e) {
   const targetDate = dayjs('Saturday October 26, 2024 23:59:59')
 
   // calculate remaining days
-  const timeDifference = targetDate.diff(dayjs(), "day")
+  const mockCurrentDate = dayjs('Saturday October 26, 2024 23:39:59')
+  let timeDifference = targetDate.diff(mockCurrentDate, "day")
+  let unit = timeDifference <= 1 ? " day" : "days"
+
+  if (timeDifference <= 1) {
+    timeDifference = targetDate.diff(mockCurrentDate, "hour")
+    unit = timeDifference === 1 ? " hour" : " hours"
+
+    if (timeDifference <= 1) {
+    timeDifference = targetDate.diff(mockCurrentDate, "minute")
+    unit = timeDifference === 1 ? " minute" : " minutes"
+    }
+  }
+
+  // let timeDifference = targetDate.diff(dayjs(), "day")
+
+  // if (timeDifference <= 1) {
+  //   timeDifference = targetDate.diff(dayjs(), "hour")
+  // }
 
   // number span
   const numberSpan = document.createElement("span")
-  numberSpan.textContent = timeDifference
+  numberSpan.innerText = timeDifference
   numberSpan.classList.add("countdown-number")
 
   // string span
   const textSpan = document.createElement("span")
-  textSpan.textContent = "  days"
+  textSpan.innerText = unit
   textSpan.classList.add("countdown-text")
 
   // append spans to container
